4afb7bc00d6eb05a75624e8bd1ebc0a674798e4e
Eric Ridenour <etridenour@gmail.com>
2026-06-23 20:55:06 -0500

    Add exercise api service

---
+++ apps/api/src/services/exercise.service.ts
@@ -0,0 +1,69 @@
+import {
+  CreateExerciseRequest,
+  Exercise,
+  UpdateExerciseRequest,
+} from "@chops/shared";
+import { prisma } from "../utils/prisma";
+import { AppError } from "../errors/AppError";
+
+export async function createExercise(
+  userId: string,
+  data: CreateExerciseRequest,
+): Promise<Exercise> {
+  const exercise = await prisma.exercise.create({
+    data: { ...data, userId },
+  });
+
+  return exercise as unknown as Exercise;
+}
+
+export async function getExercises(userId: string): Promise<Exercise[]> {
+  const exercises = await prisma.exercise.findMany({
+    where: { userId },
+  });
+
+  return exercises as unknown as Exercise[];
+}
+
+export async function getExerciseById(
+  userId: string,
+  exerciseId: string,
+): Promise<Exercise> {
+  const exercise = await prisma.exercise.findUnique({
+    where: { id: exerciseId, userId },
+  });
+
+  if (!exercise) {
+    throw new AppError(404, "Exercise not found");
+  }
+
+  return exercise as unknown as Exercise;
+}
+
+export async function updateExercise(
+  userId: string,
+  exerciseId: string,
+  data: UpdateExerciseRequest,
+): Promise<Exercise> {
+  await getExerciseById(userId, exerciseId); // throws 404 if not found
+
+  const exercise = await prisma.exercise.update({
+    where: { id: exerciseId },
+    data,
+  });
+
+  return exercise as unknown as Exercise;
+}
+
+export async function deleteExercise(
+  userId: string,
+  exerciseId: string,
+): Promise<Exercise> {
+  await getExerciseById(userId, exerciseId); // throws 404 if not found
+
+  const exercise = await prisma.exercise.delete({
+    where: { id: exerciseId, userId },
+  });
+
+  return exercise as unknown as Exercise;
+}
